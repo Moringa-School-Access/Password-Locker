f463df12d5ab829b6fcfe1061338eda636a592af
Alexotieno1717 <alexotieno900@gmail.com>
2020-09-07 01:19:29 +0300

    Added some user credentialusername, email and password

--- credential_test.py
+++ credential_test.py
@@ -15,8 +15,21 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user = User("Alexotieno", "alexotieno900@gmail", "alex123")
 
+    # def test_init(self):
+    #     '''
+    #     test_init test case to test if the object is initialized properly
+    #     '''
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+
     def test_init(self):
         '''
-        test_init test case to test if the object is initialized properly
+        test_init test case to test if object utilizes proper instantiation
         '''
-        
+        self.assertEqual(self.new_user.user_name,"Alexotieno")
+        self.assertEqual(self.new_user.email,"alexotieno900@gmail.com")
+        self.assertEqual(self.new_user.password,"alex123")
