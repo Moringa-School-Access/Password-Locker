6af12e678ce91a7bc12629742bdcefa45283bb11
Alexotieno1717 <alexotieno900@gmail.com>
2020-09-07 01:03:30 +0300

    Added class credential

--- credentials.py
+++ credentials.py
@@ -47,19 +47,19 @@ class Credential:
     class that create new instance of credential
     '''
 
-    Credential_list = [] # Empty credential list
+    credential_list = [] # Empty credential list
 
-    def __init__(self, username, password=None):
+    def __init__(self, first_name, last_name, user_name, password, email):
         '''
-        __init method help us define the property of our object
+        __init__ method help us define the property of our object
         
-        Arg:
-            username: New username.
-            password: New password.
         '''
 
-        self.username = username
-        self.password = password if password else Credential.password_generator()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.password = password
+        self.email = email
 
     def save_credential(self):
         
@@ -67,5 +67,51 @@ class Credential:
         save_credential method saves credential objects into credential_list
         
         ''' 
+
+        Credential.credential_list.append(self)
+
+    def delete_credential(self):
+        '''
+        delete_credential method delete credential saved in the credential_list
+        '''
+        Credential.credential_list.remove(self)
+
+    @classmethod
+    def find_email(cls, email):
+        '''
+        find_email method takes an email and return an email the matches the credential
+        
+        Arg:
+            email: email to search for
+        Return:
+            Credentials of person that matches the email.
+        '''
+
+        for credential in cls.credential_list:
+            if credential.email == email:
+                return credential
+    
+    @classmethod
+    def credential_exit(cls, email):
+        '''
+        Method that check if credential exit from credential_list
+
+        Arg:
+            email: email to search for email exit
+        Returns:
+            Boolean : True or False
+        '''
+        for credential in cls.credential_list:
+            if credential.email == email:
+                return True
+
+        return False
+
+    @classmethod
+    def display_credential(cls):
+        '''
+        display_credential method that retuns the list of credential_list
+        '''
+        return cls.credential_list
 
     
